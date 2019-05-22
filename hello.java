167680676961cfed7659a435a243bcd23ad4a7df
hanuma <hanu143@gmail.com>
2019-05-22 05:50:30 -0700

    modyft hello.java

--- hello.java
+++ hello.java
@@ -1,13 +1,8 @@
 class       #from dev1
 {
-
-     public static void main()    #from dev1
-
+    public static void main() #from dev1 
       {
-         system output println("hellohanumanthra from dev1");
-
 
       }
-
-}#fromdev2
+}#fromdev1
 
